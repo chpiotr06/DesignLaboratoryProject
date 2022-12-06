964e85b4abada170e953591038ef4daf389a3872
gorakj <117322895+gorakj@users.noreply.github.com>
2022-12-06 23:46:39 +0100

    Update quick-setup.sh

--- quick-setup.sh
+++ quick-setup.sh
@@ -6,7 +6,7 @@ export PICO_SDK_PATH="/home/$USERNAME/Downloads/pico-sdk"
 echo -e "Pico-SDK saved in /home/$USERNAME/Downloads"
 sleep 1
 
-git clone https://github.com/chpiotr06/DesignLaboratoryProject/tree/main --branch dev
+git clone https://github.com/chpiotr06/DesignLaboratoryProject/ --branch dev
 cd DesignLaboratoryProject
 mkdir build && cd build 
 wget https://raw.githubusercontent.com/raspberrypi/pico-sdk/master/external/pico_sdk_import.cmake
